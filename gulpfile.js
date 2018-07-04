5179c96d6a56cf6b2b2123f7a66aa0cd79c51984
nichmo <36725207+nichmo@users.noreply.github.com>
2018-07-04 23:53:47 +0900

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,14 @@ const substraction = (x, y) => {
   return x - y
 }
 substraction(5,2)
+
+
+var pug = require('gulp-pug');
+
+gulp.task('pug', () => {
+  return gulp.src(['./pug/**/*.pug', '!./pug/**/_*.pug'])
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./html/'))
+})
